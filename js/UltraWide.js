9af072292fe1b8d6297b253fe5582bfdc43d1027
Daniel Russell <danielcrussell@gmail.com>
2018-10-28 07:46:02 +0000

    SBS Split functionality, Ignore real 21:9 videos

--- js/UltraWide.js
+++ js/UltraWide.js
@@ -21,67 +21,83 @@ function remClass(video, styleClass) {
     }
 }
 
-UltraWide.prototype.update = function () {
+function getFullScreenStatus() {
+    return document.webkitCurrentFullScreenElement;
+}
+
+UltraWide.prototype.updateAspectRatio = function () {
     // Calculate scale factor:
     const aspect = screen.width / screen.height;
     if (aspect >= 1.88) { // If wider than 16:9 widescreen:
-        const scale = aspect / 1.77; this.scale = Math.round(scale * 100) / 100;
-    } else if (this.mode == 3 || this.mode == 4) this.scale = 1.33; // Force Modes
-    else this.scale = 1; // Default
+        const scale = aspect / 1.77;
+        this.scale = Math.round(scale * 100) / 100;
+    } else this.scale = 1; // Default
 
     // Update Styles:
-    this.styles.innerHTML = '.extraClassAspect { -webkit-transform:scaleX(' + this.scale + ')!important; }'
-        + '.extraClassCrop { -webkit-transform:scale(' + this.scale + ')!important; }';
+    this.styles.innerHTML = '.extraClassCrop { -webkit-transform:scale(' + this.scale + ')!important; }';
 
-    // Update Classes:
-    let fullscreen;
-
-    if (document.webkitCurrentFullScreenElement !== null) {
-        fullscreen = true;
-    } else {
-        fullscreen = false;
-    }
+    const fullscreen = getFullScreenStatus();
 
     const video = document.getElementsByTagName('video');
+    // Check if video is already Ultrawide
+    const videoAspect = video[0].videoWidth / video[0].videoHeight;
 
+    // Update Classes:
     if (video.length !== 0) {
         switch (this.mode) {
             case 0: // Disabled
-                remClass(video, 'extraClassAspect');
                 remClass(video, 'extraClassCrop');
                 break;
             case 1: // Crop
-                if (fullscreen && this.scale > 1) {
+                if (fullscreen && this.scale > 1 && videoAspect < 2.37) {
                     addClass(video, 'extraClassCrop');
-                    remClass(video, 'extraClassAspect');
                 } else {
-                    remClass(video, 'extraClassAspect');
                     remClass(video, 'extraClassCrop');
                 }
                 break;
             case 2: // Force Crop
                 addClass(video, 'extraClassCrop');
-                remClass(video, 'extraClassAspect');
                 break;
         }
     }
 
-    // Update every 12s in fullscreen mode:
+    // Update every 5s in fullscreen mode:
     if (fullscreen && this.mode > 0 && video.length > 0) {
-        if (this.timer != null) {
-            clearTimeout(this.timer);
+        if (this.aspectRatioTimer != null) {
+            clearTimeout(this.aspectRatioTimer);
         }
-        this.timer = setTimeout(function () {
-            this.update();
-            this.timer = null;
+        this.aspectRatioTimer = setTimeout(function () {
+            this.updateAspectRatio();
+            this.aspectRatioTimer = null;
         }.bind(this), 5000);
+    }
+}
+
+UltraWide.prototype.updateSBSMode = function () {
+    if (window.location.host === 'www.youtube.com') {
+        const video = document.getElementsByTagName('video');
+        const canvasMask = video[0].nextElementSibling;
+        if (canvasMask) {
+            canvasMask.hidden = this.sbsToggle;
+        }
+        // Update every 5s because sometimes the canvas element takes a moment to appear:
+        if (this.sbsToggle && video.length > 0) {
+            if (this.sbsTimer != null) {
+                clearTimeout(this.sbsTimer);
+            }
+            this.sbsTimer = setTimeout(function () {
+                this.updateSBSMode();
+                this.sbsTimer = null;
+            }.bind(this), 5000);
+        }
     }
 }
 
 function UltraWide() {
     this.mode = 0;
+    this.sbsToggle = false;
     document.addEventListener('webkitfullscreenchange', function () {
-        this.update();
+        this.updateAspectRatio();
     }.bind(this));
 
     document.addEventListener('keydown', function (hotKeyPressed) {
@@ -100,11 +116,20 @@ function onLoad() {
     const ultrawide = new UltraWide();
     chrome.storage.local.get('extensionMode', function (status) {
         ultrawide.mode = status.extensionMode;
-        if (status.extensionMode != 0) ultrawide.update();
+        if (status.extensionMode != 0) ultrawide.updateAspectRatio();
+    });
+    chrome.storage.local.get('extensionSBSToggle', function (status) {
+        ultrawide.sbsToggle = status.extensionSBSToggle;
+        if (status.extensionSBSToggle != false) ultrawide.updateSBSMode();
     });
     chrome.storage.onChanged.addListener(function (changes) {
-        ultrawide.mode = changes.extensionMode.newValue;
-        ultrawide.update();
+        if (changes.extensionMode) {
+            ultrawide.mode = changes.extensionMode.newValue;
+            ultrawide.updateAspectRatio();
+        } else if (changes.extensionSBSToggle) {
+            ultrawide.sbsToggle = changes.extensionSBSToggle.newValue;
+            ultrawide.updateSBSMode();
+        }
     });
 }
 
